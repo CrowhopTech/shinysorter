3add55733f520dc45c3f943403d68fe04c84a411
Adam Krpan <adam.krpan@outlook.com>
2023-02-05 13:14:00 -0800

    Initial commit

---
+++ apps/shinysorter-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shinysorter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shinysorter-web';
+}
