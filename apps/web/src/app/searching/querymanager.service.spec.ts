d2f6e1382f729d16b021b554488e1b02add09ac0
Adam Krpan <adam.krpan@outlook.com>
2023-05-29 01:34:59 -0700

    Added some more unit tests
    This is by no means complete, but it provides a good example for
    tests I can use to help test other classes

--- apps/web/src/app/searching/querymanager.service.spec.ts
+++ apps/web/src/app/searching/querymanager.service.spec.ts
@@ -1,14 +1,146 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { QueryManagerService } from './querymanager.service';
+import { QueryManagerService, excludeModeParam, excludeTagsParam, getNumberArrayParam, getSearchModeParam, includeModeParam, includeTagsParam, viewingFileParam } from './querymanager.service';
 import { AppConfig, TOKEN } from '../app.service';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FileQuery, SearchMode } from '../filequery';
+import { SupabaseService } from '../supabase.service';
+
+// Tests for the getNumberArrayParam function
+describe('getNumberArrayParam', () => {
+  it('should return an empty array if the parameter is undefined', () => {
+    const params = {};
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an empty array if the parameter is empty', () => {
+    const params = { asdf: "" };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an empty array if the parameter is whitespace', () => {
+    const params = { asdf: "  " };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an empty array if the parameter is whitespace with commas', () => {
+    const params = { asdf: "  , , " };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an array of numbers if the parameter is a single number', () => {
+    const params = { asdf: "123" };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([123]);
+  });
+
+  it('should return an array of numbers if the parameter is a single number with whitespace', () => {
+    const params = { asdf: " 123 " };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([123]);
+  });
+
+  it('should return an array of numbers if the parameter is a single number with whitespace and commas', () => {
+    const params = { asdf: " 123, , " };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([123]);
+  });
+
+  it('should return an array of numbers if the parameter is a single number with whitespace and commas', () => {
+    const params = { asdf: " 123, , " };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([123]);
+  });
+
+  it('should return an array of numbers if the parameter is multiple numbers with whitepsace and commas', () => {
+    const params = { asdf: " 123, , 456, 789 " };
+    const param = "asdf";
+    const result = getNumberArrayParam(params, param);
+    expect(result).toEqual([123, 456, 789]);
+  });
+
+  it('should throw an error if an invalid value is provided', () => {
+    const params = { asdf: "  lmao " };
+    const param = "asdf";
+    expect(() => getNumberArrayParam(params, param)).toThrowError(SyntaxError);
+  });
+});
+
+// Tests for the getSearchModeParam function
+// Test:
+// * Whitespace
+// * Undefined
+// * Invalid values
+// * Valid values
+// * Valid values with whitespace
+describe('getSearchModeParam', () => {
+  it('should return the default if the parameter is undefined', () => {
+    const params = {};
+    const param = "asdf";
+    const def = "all";
+    const result = getSearchModeParam(params, param, def);
+    expect(result).toEqual(def);
+  });
+
+  it('should return the default if the parameter is empty', () => {
+    const params = { asdf: "" };
+    const param = "asdf";
+    const def = "any";
+    const result = getSearchModeParam(params, param, def);
+    expect(result).toEqual(def);
+  });
+
+  it('should return the default if the parameter is whitespace', () => {
+    const params = { asdf: "  " };
+    const param = "asdf";
+    const def = "all";
+    const result = getSearchModeParam(params, param, def);
+    expect(result).toEqual(def);
+  });
+
+  it('should return "all" if the parameter is "all" with whitespace', () => {
+    const params = { asdf: "  all " };
+    const param = "asdf";
+    const def = "any";
+    const result = getSearchModeParam(params, param, def);
+    expect(result).toEqual("all");
+  });
+
+  it('should return "any" if the parameter is "any" with whitespace', () => {
+    const params = { asdf: "  any " };
+    const param = "asdf";
+    const def = "all";
+    const result = getSearchModeParam(params, param, def);
+    expect(result).toEqual("any");
+  });
+
+  it('should throw an error if an invalid value is provided', () => {
+    const params = { asdf: "  lmao " };
+    const param = "asdf";
+    const def = "all";
+    expect(() => getSearchModeParam(params, param, def)).toThrowError(SyntaxError);
+  });
+});
 
 describe('QueryManagerService', () => {
   let service: QueryManagerService;
+  let router: Router;
+  let supaClient: SupabaseService;
   let appConfig = new AppConfig();
   appConfig.supabaseAddress = "http://localhost:8080";
   appConfig.supabaseKey = "asdfasdf";
@@ -27,9 +159,76 @@ describe('QueryManagerService', () => {
       ]
     });
     service = TestBed.inject(QueryManagerService);
+    router = TestBed.inject(Router);
+    supaClient = TestBed.inject(SupabaseService);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should go to the correct URL when going to a random new file', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    service.nextRandomFile();
+    expect(navigateSpy).toHaveBeenCalledWith([]);
+  });
+
+  it('should do nothing when going to an undefined file ID', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    service.viewFile(undefined);
+    expect(navigateSpy).not.toHaveBeenCalled();
+  });
+
+  it('should navigate to the proper location when viewing a file', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    service.viewFile(1234);
+    expect(navigateSpy).toHaveBeenCalledWith([], {
+      queryParamsHandling: 'merge',
+      queryParams: {
+        [viewingFileParam]: "1234",
+      }
+    });
+  });
+
+  it('should navigate to the proper location when closing a file viewer', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    service.viewClose();
+    expect(navigateSpy).toHaveBeenCalledWith([], {
+      queryParamsHandling: 'merge',
+      queryParams: {
+        [viewingFileParam]: null,
+      }
+    });
+  });
+
+  it('should navigate to the proper location when given a file query', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    service.navigateToQuery(new FileQuery([1, 2], [3], "all", "any", true));
+    expect(navigateSpy).toHaveBeenCalledWith([], {
+      queryParams: {
+        [includeTagsParam]: "1,2",
+        [excludeTagsParam]: "3",
+        [includeModeParam]: "all",
+        [excludeModeParam]: "any",
+      }
+    });
+  });
+
+  it('should have the proper calculations for going forward and backward through images', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+
+    service.navigateToQuery(new FileQuery([1, 2], [3], "all", "any", true));
+    expect(navigateSpy).toHaveBeenCalledWith([], {
+      queryParams: {
+        [includeTagsParam]: "1,2",
+        [excludeTagsParam]: "3",
+        [includeModeParam]: "all",
+        [excludeModeParam]: "any",
+      }
+    });
   });
 });
--- apps/web/src/app/searching/searchinput/tagquery/tagquery.component.spec.ts
+++ apps/web/src/app/searching/searchinput/tagquery/tagquery.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -28,7 +28,47 @@ describe('TagqueryComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  test('should show the remove button only when expected', fakeAsync(() => {
+    let clickedTag = "";
+    component.removeClicked.subscribe(t => clickedTag = t);
+
+    component.removeVisible = true;
+    fixture.detectChanges();
+    let removeButton = fixture.debugElement.nativeElement.querySelector('#remove-button');
+    expect(removeButton).toBeTruthy();
+
+    // Click the button and ensure an event is emitted
+    removeButton.click();
+    tick();
+    expect(clickedTag).toBeTruthy();
+
+    component.removeVisible = false;
+    fixture.detectChanges();
+    removeButton = fixture.nativeElement.querySelector('#remove-button');
+    expect(removeButton).toBeFalsy();
+  }));
+
+  test('should show the add button only when expected', fakeAsync(() => {
+    let clickedTag = "";
+    component.addClicked.subscribe(t => clickedTag = t);
+
+    component.addVisible = true;
+    fixture.detectChanges();
+    let addButton = fixture.nativeElement.querySelector('#add-button');
+    expect(addButton).toBeTruthy();
+
+    // Click the button and ensure an event is emitted
+    addButton.click();
+    tick();
+    expect(clickedTag).toBeTruthy();
+
+    component.addVisible = false;
+    fixture.detectChanges();
+    addButton = fixture.nativeElement.querySelector('#add-button');
+    expect(addButton).toBeFalsy();
+  }));
 });
